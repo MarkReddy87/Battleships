529d975e64ff0530c4b9c39c3d4c17ebaa21ab3d
MarkReddy87 <markreddy87@gmail.com>
2021-08-14 12:58:13 +0000

    add white space after commas in print f statements

--- run.py
+++ run.py
@@ -8,15 +8,15 @@ def new_game():
     """
     size = 5
     num_ships = 4
-    print("<","-" * 38,">")
+    print("<", "-" * 38, ">")
     print(" Welcome to the classic Battleships game!")
     print(f" The game board is a {size} x {size} square")
     print(f" You and the computer have {num_ships} ships each")
     print(" The top left corner is Row:0 Col:0")
-    print("<","-" * 38,">")
+    print("<", "-" * 38, ">")
     player_name = input(" Please enter your name here:")
     print(f" Good luck {player_name} and remember to have fun!")
-    print("<","-" * 38,">")
+    print("<", "-" * 38, ">")
 
 
 new_game()
