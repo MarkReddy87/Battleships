68b5273bf1f5bcd164eaa93409befe4d4763d6ac
MarkReddy87 <markreddy87@gmail.com>
2021-08-15 10:00:45 +0000

    add make guess functioin

--- run.py
+++ run.py
@@ -22,26 +22,41 @@ def print_computer_board(computer_board):
         print(" ".join(row))
 
 
-def random_row(computer_board):
+"""
+choosing random row's and column's for ships on both boards and
+storing them in variables
+"""
+
+
+def comp_random_row(computer_board):
     return randint(0, len(computer_board) - 1)
 
 
-def random_col(computer_board):
+def comp_random_col(computer_board):
     return randint(0, len(computer_board[0]) - 1)
 
 
 def player_random_row(player_board):
-    return randint(0, len(player_board) - 1)
+    return randint(0, len(computer_board) - 1)
 
 
 def player_random_col(player_board):
-    return randint(0, len(player_board[0]) - 1)
+    return randint(0, len(computer_board[0]) - 1)
 
 
-comp_ship_row = random_row(computer_board)
-comp_ship_col = random_col(computer_board)
+comp_ship_row = comp_random_row(computer_board)
+comp_ship_col = comp_random_col(computer_board)
 player_ship_row = player_random_row(player_board)
 player_ship_col = player_random_col(player_board)
+
+
+def make_guess():
+    guess_row = input("\nPlease guess a row: ")
+    guess_col = input("Please guess a column: ")
+    print(f"\nYou have guessed row:{guess_row} and column:{guess_col}")
+    comp_guess_row = randint(0, len(computer_board) - 1)
+    comp_guess_col = randint(0, len(computer_board[0]) - 1)
+    print(f"Computer has guessed row:{comp_guess_row} and cloumn:{comp_guess_col}")
 
 
 def new_game():
@@ -65,6 +80,8 @@ def new_game():
     print_player_board(player_board)
     print("\nComputer's Board:\n")
     print_computer_board(computer_board)
+
+    make_guess()
 
 
 new_game()
